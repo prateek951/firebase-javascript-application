31714949a80f0f9588cec9de21a52a211f5c2516
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2019-01-07 18:08:51 +0530

    login and logout user code

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,13 @@
 const registerForm = document.querySelector("#signup-form");
+const loginForm = document.querySelector("#login-form");
 let registerModal;
+let loginModal;
+let logout;
 setReferences();
 bindEvents();
+/**
+ * Code to register the user *
+ *  */
 
 async function registerUser(e) {
   e.preventDefault();
@@ -17,16 +23,51 @@ async function registerUser(e) {
   );
   // Get the user from the received credentials
   const { user } = credentials;
+  console.log(user);
   //Clear the form
   registerForm.reset();
   //Close the Modal
-   M.Modal.getInstance(registerModal).close();
+  M.Modal.getInstance(registerModal).close();
 }
 
+/**
+ * Code to log the user in
+ *
+ */
+async function loginUser(e) {
+  e.preventDefault();
+  //Tap credentials
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value; 
+
+  //Reach out to firebase
+  const credentials = await auth.signInWithEmailAndPassword(email,password);
+  console.log(credentials);
+  //Reset the login form
+  loginForm.reset();
+  //Close the modal 
+  M.Modal.getInstance(loginModal).close();
+}
+
+/**
+ * Code to log the user out
+ */
+async function logoutUser(e) {
+  e.preventDefault();
+  //log me out
+  const result = await auth.signOut();
+  console.log(`You are now logged out ${result}`);
+}
+//Bind the events
 function bindEvents() {
   registerForm.addEventListener("submit", registerUser);
+  loginForm.addEventListener("submit", loginUser);
+  logout.addEventListener("click", logoutUser);
 }
-
+//Set the references
 function setReferences() {
   registerModal = document.querySelector("#modal-signup");
+  loginModal = document.querySelector('#modal-login');
+  logout = document.querySelector("#logout");
+
 }
