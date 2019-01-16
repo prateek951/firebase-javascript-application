d8cf00d87a15e4cfee35c018d9601d67d6e174be
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2019-01-16 18:31:25 +0530

    setting firestore listener and getting accountDetails

--- js/auth.js
+++ js/auth.js
@@ -2,23 +2,32 @@
 auth.onAuthStateChanged(user => {
   console.log(`Status of user` + user);
   if (user) {
+    // setup the user interface for the navbar show only the logged in links
+    setUserInterface(user);
     //get the data only if the user is logged in.
     //pull out the docs from the snapshot
-    db.collection("guides")
-      .get()
-      .then(({ docs }) => {
-        setGuides(docs);
-      });
+    db.collection("guides").onSnapshot(({ docs }) => {
+      setGuides(docs);
+    }).catch(ex => console.log(ex.message));
   } else {
+    //setup the user interface for the navbar show only the logged out links
+    setUserInterface(null);
     //In case the user does not exists, auth does not exist then no call
     setGuides([]);
   }
 });
 
+/** form references */
 const registerForm = document.querySelector("#signup-form");
 const loginForm = document.querySelector("#login-form");
+const createForm = document.querySelector("#create-form");
+/** 
+ * modal references
+ * */
 let registerModal;
+let accountModal;
 let loginModal;
+let guideModal;
 let logout;
 setReferences();
 bindEvents();
@@ -28,23 +37,27 @@ bindEvents();
 
 async function registerUser(e) {
   e.preventDefault();
-  // console.log("submit event");
-  // Tap the credentials
-  const email = registerForm["signup-email"].value;
-  const password = registerForm["signup-password"].value;
-  // console.log(email, password);
-  //register the user
-  const credentials = await auth.createUserWithEmailAndPassword(
-    email,
-    password
-  );
-  // Get the user from the received credentials
-  const { user } = credentials;
-  console.log(user);
-  //Clear the form
-  registerForm.reset();
-  //Close the Modal
-  M.Modal.getInstance(registerModal).close();
+  try {
+    // console.log("submit event");
+    // Tap the credentials
+    const email = registerForm["signup-email"].value;
+    const password = registerForm["signup-password"].value;
+    // console.log(email, password);
+    //register the user
+    const credentials = await auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+    // Get the user from the received credentials
+    const { user } = credentials;
+    console.log(user);
+    //Clear the form
+    registerForm.reset();
+    //Close the Modal
+    M.Modal.getInstance(registerModal).close();
+  } catch (ex) {
+    console.log("an error occurred", ex);
+  }
 }
 
 /**
@@ -53,17 +66,21 @@ async function registerUser(e) {
  */
 async function loginUser(e) {
   e.preventDefault();
-  //Tap credentials
-  const email = loginForm["login-email"].value;
-  const password = loginForm["login-password"].value;
+  try {
+    //Tap credentials
+    const email = loginForm["login-email"].value;
+    const password = loginForm["login-password"].value;
 
-  //Reach out to firebase
-  const credentials = await auth.signInWithEmailAndPassword(email, password);
-  console.log(credentials);
-  //Reset the login form
-  loginForm.reset();
-  //Close the modal
-  M.Modal.getInstance(loginModal).close();
+    //Reach out to firebase
+    const credentials = await auth.signInWithEmailAndPassword(email, password);
+    console.log(credentials);
+    //Reset the login form
+    loginForm.reset();
+    //Close the modal
+    M.Modal.getInstance(loginModal).close();
+  } catch (ex) {
+    console.log(ex);
+  }
 }
 
 /**
@@ -71,19 +88,45 @@ async function loginUser(e) {
  */
 async function logoutUser(e) {
   e.preventDefault();
-  //log me out
-  const result = await auth.signOut();
-  console.log(`You are now logged out ${result}`);
+  try {
+    //log me out
+    const result = await auth.signOut();
+    console.log(`You are now logged out ${result}`);
+  } catch (ex) {
+    console.log("an error occurred", ex);
+  }
+}
+/**
+ * Code to create a new guide
+ */
+async function makeGuide(e) {
+  e.preventDefault();
+  try {
+    //get the form data and make a guide then the call to firestore
+    const result = await db.collection("guides").add({
+      title: createForm["title"].value,
+      content: createForm["content"].value
+    });
+    console.log(result);
+    //Reset the form and clear the modal
+    createForm.reset();
+    //Close the modal
+    M.Modal.getInstance(guideModal).close();
+  } catch (ex) {
+    console.log(`an error occurred` + ex.message);
+  }
 }
 //Bind the events
 function bindEvents() {
   registerForm.addEventListener("submit", registerUser);
   loginForm.addEventListener("submit", loginUser);
   logout.addEventListener("click", logoutUser);
+  createForm.addEventListener("submit", makeGuide);
 }
 //Set the references
 function setReferences() {
   registerModal = document.querySelector("#modal-signup");
   loginModal = document.querySelector("#modal-login");
+  guideModal = document.querySelector("#modal-create");
   logout = document.querySelector("#logout");
 }
--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", init);
 //set the references
 let guides;
+// Links that are shown only to authenticated users
+let loggedInLinks;
+//links that are shown only to unauthenticated users
+let loggedOutLinks;
+let accountDetails;
 setRefs();
 
 const setGuides = docs => {
@@ -32,10 +37,31 @@ function init() {
   var items = document.querySelectorAll(".collapsible");
   M.Collapsible.init(items);
 }
-//any setting of references will go here 
+//any setting of references will go here
 function setRefs() {
   guides = document.querySelector(".guides");
+  loggedInLinks = document.querySelectorAll(".logged-in");
+  loggedOutLinks = document.querySelectorAll(".logged-out");
+  accountDetails = document.querySelector(".account-details");
+}
+
+function setUserInterface(user) {
+  if (user) {
+    //account details
+    let html = `
+      <h4>Logged in as ${user.email}</h4>
+    `;
+    accountDetails.innerHTML = html;
+    loggedInLinks.forEach(link => (link.style.display = "block"));
+    loggedOutLinks.forEach(link => (link.style.display = "none"));
+  } else {
+    //hide account details
+    accountDetails.innerHTML = "";
+    loggedInLinks.forEach(link => (link.style.display = "none"));
+    loggedOutLinks.forEach(link => (link.style.display = "block"));
+  }
 }
 
 //Any binding of events will go here
-function bindEvents() {}
+function bindEvents() {
+}
